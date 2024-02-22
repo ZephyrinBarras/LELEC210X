ec4ac4a3c688cc7b78f8fc6b6e3276de834bb62c
barrasz <zephyrin.barras@student.uclouvain.be>
2024-02-22 15:16:09 +0100

    authentif packet.c

--- mcu/hands_on_main_app/Core/Src/packet.c
+++ mcu/hands_on_main_app/Core/Src/packet.c
@@ -27,16 +27,11 @@ void tag_cbc_mac(uint8_t *tag, const uint8_t *msg, size_t msg_len) {
     // TO DO : Complete the CBC-MAC_AES
 
 
-    for (i=0; i<msg_len/16; i++){
-    	for (int j =0; j<16; j++){
-    		if (16*i+j<len){
-    			state[j] = state[16*i+j] ^ msg[16*i+j];
-    		}else{
-    			state[j] = state[16*i+j];
-    		}
 
+    for (i=0; i<(msg_len/16); i++){
+    	for (int j =0; j<16 && 16*i+j<msg_len; j++){
+    		state[j]^=msg[i*16+j];
     	}
-
     	AES128_encrypt( state, (unsigned char*) AES_Key);
     }
     // Copy the result of CBC-MAC-AES to the tag.
