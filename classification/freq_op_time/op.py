f0742be9995fc05bceec8cf0fa661882b92ea793
barrasz <zephyrin.barras@student.uclouvain.be>
2024-03-05 14:59:04 +0100

    operation vs time slice and frequency

---
+++ classification/freq_op_time/op.py
@@ -0,0 +1,35 @@
+import numpy as np
+from numpy import log10
+import matplotlib.pyplot as plt
+
+BASE_TIME = 0.5
+BASE_FREQ = 10e3
+T = 1/BASE_FREQ
+NBR_ECHAN = BASE_TIME/T
+plt.figure()
+
+def ope(n):
+    return (n*log10(n))
+
+def plot_op_time():
+    time = np.linspace(0.2,1,1000)
+    echan = time*BASE_FREQ
+    print(time)
+    print(ope(echan)/time)
+    plt.plot(time, ope(echan)/time)
+    plt.show()
+
+def plot_op_freq():
+    freq = np.linspace(5000,20000,1000)
+    echan = BASE_TIME*freq
+    plt.plot(freq, ope(echan)/BASE_TIME)
+    plt.show()
+
+def plot_op(freq, time):
+    echan = time*freq
+    print(f"base : \t {ope(NBR_ECHAN)/BASE_TIME} perso : \t {ope(echan)/time}")
+
+
+plot_op_time()
+plot_op_freq()
+
