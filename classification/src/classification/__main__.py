e9eae33c02d554890755751bb4f0c3ca47c7ec43
barrasz <zephyrin.barras@student.uclouvain.be>
2024-04-19 14:09:27 +0200

    bipbip demo3

--- classification/src/classification/__main__.py
+++ classification/src/classification/__main__.py
@@ -19,28 +19,32 @@ from sklearn.impute import SimpleImputer
 
 from .utils import payload_to_melvecs
 
+listeclass=["birds","chainsaw","fire","handsaw","helicopter"]
+name = sorted(listeclass)
+max_size = 4 #à changer
+memory = zeros((max_size,5))
+pos = 0
+commencement=0
+def ajouter(elem):
+    global pos
+    memory[pos] = elem
+    pos = (pos+1)%max_size
+    
+def get_prob(prob):
+    result =mean(memory)
+    print(result)
+    return mean(memory)
+
+def result():
+    return argmax(get_prob())
+
+
+
 load_dotenv()
 pca = pickle.load(open("classification/data/models/pca_vite","rb"))
 model = pickle.load(open("classification/data/models/model_vite","rb"))
 
-plt.figure()
-#plt.show()
 
-def myfct(feature_vector,n_melvecs, melvec_length, counter):
-    #plt.figure(clear=True)
-    feature_vector = feature_vector.reshape(-1)
-    #logger.info(f"Parsed payload into Mel vectors: {melvecs}")
-    mp = pca.transform([feature_vector[:-20]])
-    #logger.info(f"Parsed payload into Mel vectors: {mp}")
-    classe = model.predict(mp)
-    #print(f"classe:\t{classe}")
-    #print(feature_vector.reshape((n_melvecs, melvec_length)).T)
-    
-    #logger.info(f"Parsed payload into Mel vectors: {classe}")
-    """plot_specgram(feature_vector.reshape((n_melvecs, melvec_length)).T, ax=plt.gca(), is_mel=True, title="MEL Spectrogram #{}".format(counter), xlabel="Mel vector")
-    plt.draw()
-    plt.pause(0.01)
-    plt.clf()"""
 
 def send(classe):
     hostname = "http://lelec210x.sipr.ucl.ac.be/lelec210x/"
@@ -88,7 +92,6 @@ def main(
     (standard input, i.e., the terminal).
     """
     imp = SimpleImputer(missing_values=nan, strategy="mean")
-    model = pickle.load(open("classification/data/models/random_forest_Q1_parameters.pickle","rb"))
     data = zeros((20,20))
     data2 = None
     first=0
@@ -107,8 +110,8 @@ def main(
     for payload in _input:
         nbr_packet+=1
         
+        
         if PRINT_PREFIX in payload:
-            logger.info(f"detect")
             previous = 0
 
             #RECUPERE ET TRAITE LE MEL
@@ -117,7 +120,6 @@ def main(
             packet_number = int(payload[len(PRINT_PREFIX):len(PRINT_PREFIX)+7],16)%20
             payload = payload[len(PRINT_PREFIX)+7:]
             melvecs = payload_to_melvecs(payload, melvec_length, 1)
-            print(packet_number)
 
             if are_error:
                 print("error")
@@ -131,100 +133,49 @@ def main(
                 previous = packet_number
                 continue
             else:
-                print("ici", packet_number) 
                 previous = packet_number
                 data[packet_number] = melvecs.reshape(-1)
                 #print(melvecs.reshape(-1))
 
             if (packet_number==19 ):
-                #melvecs1 = data.reshape(-1)
+                pca = pickle.load(open("classification/data/models/pca_vite","rb"))
+                model = pickle.load(open("classification/data/models/model_vite","rb"))
                 print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-                #print(data)
                 data = imp.fit_transform(data)
-                #print(data)
-                mp = pca.transform([data.reshape(-1)[:-20]])
+                data = data-mean(data)
+                mp = data/linalg.norm(data, axis=1,keepdims=True)
+                mp = pca.transform([data.reshape(-1)])
+                
                 #plt.imshow(data)
-                classe = model.predict(mp)
-                print(classe)
+                elem = model.predict_log_proba(mp)[0]
+                ajouter(elem)
+                global commencement
+                commencement+=1
+                probs=memory
+                maxvrai= zeros(5) 
+                for m in range(5) :
+                    for n in range(len(probs)):
+                        maxvrai[m]=+probs[n,m]
+                #pour le seuille
+                """seuille=0.6
+                if max(10**(maxvrai/5))<seuille:
+                    classe=" "
+                else:
+                    classe=name[argmax(maxvrai)]"""
+                print(max(10**(maxvrai/5)))
+                classe=name[argmax(maxvrai)]
+        
+            
+            
+                logger.info(f"Parsed payload into Mel vectors: {classe}")
                 
 
 
-                """plot_specgram(data.reshape((20, 20)).T, ax=plt.gca(), is_mel=True,
+                plot_specgram(data.reshape((20, 20)).T, ax=plt.gca(), is_mel=True,
                           title="MEL Spectrogram", xlabel="Mel vector", tf=512 / 11000)
                 plt.draw()
                 plt.pause(0.05)
-                plt.clf()"""
-                send(classe)
+                plt.clf()
+                #send(classe)
                 data = full((20, 20), nan)
                 main()
-            
-
-                
-
-                
-
-                
-
-
-            first=1
-            #logger.info(f"Parsed payload into Mel vectors: {melvecs}")
-            #logger.info(str(len(melvecs))+str( len(melvecs[0])))
-            #logger.info("ananas")
-            #melvecs1 = melvecs.reshape(-1)
-            #logger.info(f"Parsed payload into Mel vectors: {melvecs}")
-            #mp = pca.transform([melvecs1[:-20]])
-            #logger.info(f"Parsed payload into Mel vectors: {mp}")
-            #classe = model.predict(mp)
-            #logger.info(f"Parsed payload into Mel vectors: {classe}")
-            
-            #plt.show()
-
-            
-
-            """hostname = "http://130.104.96.147:5000"
-            key = "QMJf02nSY3SvesXt1I8Vi9bKZT520NmM4LpIhHNN"
-            guess = classe
-
-            response = requests.post(f"{hostname}/lelec210x/leaderboard/submit/{key}/{guess}")"""
-
-            
-
-            """if done==0:
-                
-                t0 = time.time()
-                if lastmissFlag:
-                    lastmissFlag=False
-                    data[0] = data2
-                    done+=1
-
-
-            #ASSIGNE LE MEL DANS LE FEATURE VECTOR
-            #!!!!!!!!! 2 erreurs de réception d'affilé non géré !!!!!!!!!!
-            
-            t1 = time.time()
-            if done<n_melvecs-1:
-                if t1-t0>0.1:
-                    nbr = (t1-t0-0.05)//0.05
-                    error.append(nbr)
-                    #print(f"delta t:\t{t1-t0:.4f}\tt0:\t{t0:.4f}")
-                    #print("packet missing (replace)")
-                    missing +=1
-                    
-                    data[done] = mean(data[0:done])#remplacer par le moyenne
-                    done +=1
-                    if done ==n_melvecs-1:
-                        lastmissFlag=True
-                        data2 = melvecs.T[0]
-                        done = 0
-                    else:
-                        data[done] = melvecs.T[0]
-                done +=1
-            else:
-                data[done] = melvecs.T[0]
-                done =0
-                nbr_nmel+=1 
-                #myfct(data,n_melvecs,melvec_length, nbr_nmel)
-                if nbr_nmel==100:
-                    pickle.dump(error, open("./error_distrib", 'wb'))
-                    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-            t0=t1"""
