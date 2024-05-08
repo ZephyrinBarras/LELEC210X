7ace979188367cdb108f881fd25491649a7c127b
barrasz <zephyrin.barras@student.uclouvain.be>
2024-05-08 09:31:57 +0200

    Vas-y gogog

--- mcu/hands_on_main_app/Core/Src/stm32l4xx_hal_msp.c
+++ mcu/hands_on_main_app/Core/Src/stm32l4xx_hal_msp.c
@@ -21,6 +21,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+
 /* USER CODE BEGIN Includes */
 
 /* USER CODE END Includes */
@@ -63,7 +64,6 @@
   */
 void HAL_MspInit(void)
 {
-
   /* USER CODE BEGIN MspInit 0 */
 
   /* USER CODE END MspInit 0 */
