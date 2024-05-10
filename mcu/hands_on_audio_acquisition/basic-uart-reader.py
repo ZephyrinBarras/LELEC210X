a17f74ce19903d2fe6ca265c348c791c1f1ac062
barrasz <zephyrin.barras@student.uclouvain.be>
2024-05-10 10:33:40 +0200

    go

--- mcu/hands_on_audio_acquisition/basic-uart-reader.py
+++ mcu/hands_on_audio_acquisition/basic-uart-reader.py
@@ -10,6 +10,24 @@ import time
 import pickle
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+listeclass=["birds","chainsaw","fire","handsaw","helicopter"]
+name = sorted(listeclass)
+max_size = 4 #à changer
+memory = np.zeros((max_size,5))
+pos = 0
+commencement=0
+def ajouter(elem):
+    global pos
+    memory[pos] = elem
+    pos = (pos+1)%max_size
+    
+def get_prob(prob):
+    result =np.mean(memory)
+    print(result)
+    return np.mean(memory)
+
+def result():
+    return np.argmax(get_prob())
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-p", "--port", help="Port for serial communication")
@@ -30,11 +48,13 @@ else :
     )
     serialString = ""  # Used to hold data coming over UART
     count = 0
-    max_count = 100
+    max_count = 360
     data = []
-    rep = []
     model = pickle.load(open("./classification/model_quick.pickle","rb"))
     parity= 0
+    memory_size = 5
+    memory_value = np.zeros((memory_size,len(name)))
+    position = 0
     while 1:
         # Wait until there is data waiting in the serial buffer
         if serialPort.in_waiting > 0:
@@ -52,20 +72,43 @@ else :
                 print(str(count/max_count*100)+"%")
                 data.append(text)
                 
-                temp = text[len("[MEAN]"):].strip("\n").strip(",")
+                """temp = text[len("[MEAN]"):].strip("\n").strip(",")
                 temp = temp.split(",")
                 for j in range(len(temp)):
                     temp[j] = np.int16(temp[j])
                 temp = np.array(temp)
                 temp =temp/np.linalg.norm(temp)
-                feature[parity] = temp
-                parity = (parity+1)%2
-                if parity==0:
-                    print(model.predict([np.ravel(feature)]))
+                feature[parity] = temp"""
+                
+                
+                """elem = model.predict_proba([temp])
+                if np.max(elem)>0.8:
+                    print("je déconne à fond")
+                    print(name[np.argmax(elem)])
+                loga = np.log(elem)
+                memory_value[position] = loga
+                print(memory_value)
+
+                position=(position+1)%5
+                proba_sum = np.sum(memory_value, axis=0)
+                retour = np.exp(proba_sum/5)
+                print(retour.shape)
+                print("variance", np.var(retour))
+                var = np.var(retour)
+
+                if np.max(np.exp(proba_sum/5))>0.6:
+                    print("nouveau threshold")
+
+                trie = np.flip(np.sort(retour))
+                print("triée ,", trie)
+                if np.var(var>0.2 and trie[0]-trie[1]>0.2):
+                    print("treash variance")
+                if np.var(trie[0]-trie[1]>0.3):
+                    print("treash diff max")"""
+
             if count==max_count:
                 break
 
-    print(rep)
     for i in range(len(data)):
         temp = data[i]
         temp = temp[len("[MEAN]"):].strip("\n").strip(",")
@@ -73,8 +116,6 @@ else :
         for j in range(len(temp)):
             temp[j] = np.int16(temp[j])
         temp = np.array(temp)
-        print(temp)
-        print(temp.astype(np.float64))
         temp =temp/np.linalg.norm(temp)
         data[i] = temp
     data = np.array(data)
