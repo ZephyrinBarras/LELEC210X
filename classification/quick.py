37e63e3fc8e1112c0471bb7babfb83b1734ff00c
valer <valerian.dosimont@student.uclouvain.be>
2024-05-16 14:48:47 +0200

    Dernières modifications basic-uart-reader.py

--- classification/quick.py
+++ classification/quick.py
@@ -8,23 +8,23 @@ import pickle
 import numpy as np
 import librosa
 from scipy.signal import fftconvolve
-
 from classification.utils.utils import accuracy
-helico = pickle.load(open("./helicopter_yt.pickle", "rb"))[0]
-helico1 = pickle.load(open("./helicopter.pickle", "rb"))[0]
+
+helico = pickle.load(open("../helicopter_yt.pickle", "rb"))[0]
+helico1 = pickle.load(open("../helicopter.pickle", "rb"))[0]
 cls_hel =np.array(["helicopter" for _ in range((len(helico)+len(helico1))//2)])
 print(cls_hel.shape, helico.shape, helico1.shape)
-birds = pickle.load(open("./birds_yt.pickle", "rb"))[0]
-birds1 = pickle.load(open("./birds.pickle", "rb"))[0]
+birds = pickle.load(open("../birds_yt.pickle", "rb"))[0]
+birds1 = pickle.load(open("../birds.pickle", "rb"))[0]
 cls_b =np.array(["birds" for _ in range((len(birds)+len(birds1))//2)])
-fire = pickle.load(open("./fire_yt.pickle", "rb"))[0]
-fire1 = pickle.load(open("./fire.pickle", "rb"))[0]
+fire = pickle.load(open("../fire_yt.pickle", "rb"))[0]
+fire1 = pickle.load(open("../fire.pickle", "rb"))[0]
 cls_f =np.array(["fire" for _ in range((len(fire)+len(fire1))//2)])
-chain = pickle.load(open("./chainsaw_yt.pickle", "rb"))[0]
-chain1 = pickle.load(open("./chainsaw.pickle", "rb"))[0]
+chain = pickle.load(open("../chainsaw_yt.pickle", "rb"))[0]
+chain1 = pickle.load(open("../chainsaw.pickle", "rb"))[0]
 cls_c =np.array(["chainsaw" for _ in range((len(chain)+len(chain1))//2)])
-hand = pickle.load(open("./handsaw_yt.pickle", "rb"))[0]
-hand1 = pickle.load(open("./handsaw.pickle", "rb"))[0]
+hand = pickle.load(open("../handsaw_yt.pickle", "rb"))[0]
+hand1 = pickle.load(open("../handsaw.pickle", "rb"))[0]
 cls_h=np.array(["handsaw" for _ in range((len(hand)+len(hand1))//2)])
 
 
@@ -55,12 +55,53 @@ for i in range(len(new_dataset)):
         new_dataset[i] = np.ravel(temp)
 X_train, X_test, y_train, y_test = train_test_split(new_dataset, data2_list, test_size=0.3)
 
-n_trees = 100
-model = RandomForestClassifier(n_trees)
+# n_trees = 100
+# model = RandomForestClassifier(n_trees)
+
+n_estimators_begin = 50
+n_estimators_end = 5000
+n_estimators_step = 250
+
+parameter_begin = 5
+parameter_end = 500
+parameter_step = 10
+
+parameters_range = np.arange(parameter_begin, parameter_end, parameter_step)  # ICI LA RANGE DU PARAM MELVEC_LENGTH
+
+# La création des matrices a été adaptée à la modification des boucles i et j
+accuracy_matrix = np.zeros(len(parameters_range))
+
+for i in parameters_range:
+
+        temp_acc_split = [] # une accuracy par split
+        n_splits = 3
+        for _ in range(n_splits):
+                X_train, X_test, y_train, y_test = train_test_split(new_dataset, data2_list, test_size=0.3)
+                # print("Taille de X_test et de y_test : ", len(X_test), len(y_test))
+                # print("Taille de X_train et de y_train : ", len(X_train), len(y_train))
+
+                "Modèle"
+                model = RandomForestClassifier(n_estimators=i, max_depth=85)
+
+                "Entraîner le modèle et la PCA"
+                model.fit(X_train, y_train)
+                prediction = model.predict(X_test)
+
+                temp_acc_split.append(accuracy(prediction, y_test))
+
+        mean_acc = np.mean(temp_acc_split)
+        accuracy_matrix[(i - parameter_begin) // parameter_step] = mean_acc
+
+        print("parameters : {}, mean accuracy of the {}-splits : {}".format(i, n_splits,100 * mean_acc))
+        print("=====================================")
+
+plt.plot(parameters_range, accuracy_matrix)
+plt.show()
 
 
-#model.fit(X_train, y_train)
-model.fit(X_train, y_train)
-prediction = model.predict(X_test)
-print(accuracy(prediction, y_test))
-pickle.dump(model,open("model_def.pickle","wb"))
+
+# #model.fit(X_train, y_train)
+# model.fit(X_train, y_train)
+# prediction = model.predict(X_test)
+# print(accuracy(prediction, y_test))
+# pickle.dump(model,open("model_def.pickle","wb"))
--- mcu/hands_on_audio_acquisition/basic-uart-reader.py
+++ mcu/hands_on_audio_acquisition/basic-uart-reader.py
@@ -5,29 +5,42 @@ ELEC PROJECT - 210x
 """
 import argparse
 import serial
-from   serial.tools import list_ports
+from serial.tools import list_ports
 import time
 import pickle
 import numpy as np
+import requests
 from sklearn.ensemble import RandomForestClassifier
-listeclass=["birds","chainsaw","fire","handsaw","helicopter"]
+
+listeclass = ["birds", "chainsaw", "fire", "handsaw", "helicopter"]
 name = sorted(listeclass)
-max_size = 4 #à changer
-memory = np.zeros((max_size,5))
+max_size = 4  # à changer
+memory = np.zeros((max_size, 5))
 pos = 0
-commencement=0
+commencement = 0
+
+
 def ajouter(elem):
     global pos
     memory[pos] = elem
-    pos = (pos+1)%max_size
-    
+    pos = (pos + 1) % max_size
+
+
 def get_prob(prob):
-    result =np.mean(memory)
+    result = np.mean(memory)
     print(result)
     return np.mean(memory)
 
+
 def result():
     return np.argmax(get_prob())
+
+
+def send(classe):
+    hostname = "http://lelec210x.sipr.ucl.ac.be/lelec210x/"
+    key = "QMJf02nSY3SvesXt1I8Vi9bKZT520NmM4LpIhHNN"
+    response = requests.post(f"{hostname}/leaderboard/submit/{key}/{classe}", timeout=1)
+
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-p", "--port", help="Port for serial communication")
@@ -42,7 +55,7 @@ if args.port is None:
         print(p.device)
     print("================")
     print("Launch this script with [-p PORT_REF] to access the communication port")
-else :
+else:
     serialPort = serial.Serial(
         port=args.port, baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE
     )
@@ -50,23 +63,25 @@ else :
     count = 0
     max_count = 300
     data = []
-    model = pickle.load(open("./model_def.pickle","rb"))
-    parity= 0
+    model = pickle.load(open("./model_def.pickle", "rb"))
+    parity = 0
     memory_size = 5
-    memory_value = np.zeros((memory_size,len(name)))
+    memory_value = np.zeros((memory_size, len(name)))
     position = 0
     vote = np.zeros(5)
     classe_vote = []
+
 
     def voteur(array):
         global vote, name
         for k in range(len(vote)):
             count = 0
             for i in array:
-                if i==name[k]:
-                    count+=1
+                if i == name[k]:
+                    count += 1
             vote[k] = count
-    
+
+
     while 1:
         # Wait until there is data waiting in the serial buffer
         if serialPort.in_waiting > 0:
@@ -75,50 +90,55 @@ else :
             serialString = serialPort.readline()
 
             # Print the contents of the serial data
-            text =serialString.decode("Ascii")
-            feature=np.zeros((2,24))    #24=MEL_LENGTH
-            
+            text = serialString.decode("Ascii")
+            feature = np.zeros((2, 24))  # 24=MEL_LENGTH
+
             if "[MEAN]" in text:
-                
-                count +=1
+
+                count += 1
                 data.append(text)
-                
+
                 temp = text[len("[MEAN]"):].strip("\n").strip(",")
                 temp = temp.split(",")
                 for j in range(len(temp)):
                     temp[j] = np.int16(temp[j])
                 temp = np.array(temp)
-                temp =temp/np.linalg.norm(temp)
+                temp = temp / np.linalg.norm(temp)
                 feature[parity] = temp
-                parity = (parity+1)%2
+                parity = (parity + 1) % 2
                 if parity:
                     elem = model.predict_proba([np.ravel(feature)])
                     predict = model.predict([np.ravel(feature)])
                     print(elem, model.predict([np.ravel(feature)]))
                     classe_vote.append(predict[0])
-                    if len(classe_vote)>10:
-                        del(classe_vote[0])
+                    if len(classe_vote) > 10:
+                        del (classe_vote[0])
                     voteur(classe_vote)
-                    if np.max(vote)>=8:
+
+                    if np.max(vote) >= 5:
                         print("critere vote")
-                        print(name[np.argmax(vote)])
-                    if np.max(elem)>0.7:
+                        # print("Envoie de ", name[np.argmax(vote)])
+
+
+                    if np.max(elem) > 0.7:
                         print("je déconne à fond")
                     loga = np.log(elem)
                     memory_value[position] = loga
 
-                    position=(position+1)%5
+                    position = (position + 1) % 5
                     proba_sum = np.sum(memory_value, axis=0)
-                    retour = np.exp(proba_sum/5)
+                    retour = np.exp(proba_sum / 5)
                     var = np.var(retour)
 
-                    if np.max(np.exp(proba_sum/5))>0.6:
+                    if np.max(np.exp(proba_sum / 5)) > 0.4:
                         print("nouveau threshold")
+                        print("Envoie de ", name[np.argmax(vote)])
+                        send(name[np.argmax(vote)])
 
                     trie = np.flip(np.sort(retour))
-                    if np.var(var>0.2 and trie[0]-trie[1]>0.2):
+                    if np.var(var > 0.2 and trie[0] - trie[1] > 0.2):
                         print("treash variance")
-                    if np.var(trie[0]-trie[1]>0.3):
+                    if np.var(trie[0] - trie[1] > 0.3):
                         print("treash diff max")
 
     for i in range(len(data)):
@@ -128,9 +148,9 @@ else :
         for j in range(len(temp)):
             temp[j] = np.int16(temp[j])
         temp = np.array(temp)
-        temp =temp/np.linalg.norm(temp)
+        temp = temp / np.linalg.norm(temp)
         data[i] = temp
     data = np.array(data)
     classe = input("enter the class recorded>>")
     name = input("enter the file name>>")
-    pickle.dump((data,classe), open(name+".pickle", "wb"))
+    pickle.dump((data, classe), open(name + ".pickle", "wb"))
